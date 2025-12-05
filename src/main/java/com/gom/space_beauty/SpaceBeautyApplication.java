0f5ec11f6f63394e5d2923cac5806895951928ac
gomes <joaogomesleite800@gmail.com>
2025-12-05 13:58:40 -0300

    initializing project

---
+++ src/main/java/com/gom/space_beauty/SpaceBeautyApplication.java
@@ -0,0 +1,13 @@
+package com.gom.space_beauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceBeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceBeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gom/space_beauty/SpaceBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.space_beauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceBeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
