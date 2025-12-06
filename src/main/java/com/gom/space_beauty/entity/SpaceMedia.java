35be43f21183216507a79b03a1b3e7c708e3236c
gomes <joaogomesleite800@gmail.com>
2025-12-06 11:19:24 -0300

    feat: entity user and favorite logic

--- src/main/java/com/gom/space_beauty/entity/SpaceMedia.java
+++ src/main/java/com/gom/space_beauty/entity/SpaceMedia.java
@@ -1,9 +1,14 @@
 package com.gom.space_beauty.entity;
 
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,4 +28,7 @@ public class SpaceMedia {
     private String type;
     private String title;
     private String copyright;
+
+    @ManyToMany(mappedBy = "favorites")
+    private List<User> favoredBy;
 }
---
+++ src/main/java/com/gom/space_beauty/entity/User.java
@@ -0,0 +1,69 @@
+package com.gom.space_beauty.entity;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate birthDate;
+
+    @ManyToMany()
+    @JoinTable(name = "tb_user_favorites", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "space_media_id"))
+    private List<SpaceMedia> favorites;
+
+    private String email;
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
